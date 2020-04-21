1390a211862573956a19d787dbbdc533c3e0a987
Adam Makarucha <adammakarucha@gmail.com>
2020-04-22 00:34:32 +1000

    Added the abililty to specify a new sources.list

---
+++ base-notebook/config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$SOURCE_LIST" != "empty" ]
+then
+	echo "Creating sources.list"
+	echo "deb $SOURCE_LIST bionic main restricted multiverse" >> /tmp/sources.list
+	echo "deb $SOURCE_LIST bionic-updates main restricted multiverse" >> /tmp/sources.list
+	echo "deb $SOURCE_LIST bionic universe" >> /tmp/sources.list
+	echo "deb $SOURCE_LIST bionic-updates universe" >> /tmp/sources.list
+	echo "deb $SOURCE_LIST bionic-security main restricted multiverse" >> /tmp/sources.list
+	echo "deb $SOURCE_LIST bionic-security universe" >> /tmp/sources.list
+	echo "Replacing Sources.list"
+	cp /tmp/sources.list /etc/apt/sources.list
+fi
+
