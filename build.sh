b33c745e9e80b83522ffbd51902bfbe1fd6212ac
Adam Makarucha <adammakarucha@gmail.com>
2020-05-10 02:57:50 +1000

    Remove wmlce kernel

--- build.sh
+++ build.sh
@@ -25,6 +25,7 @@ do
         then
             echo $ARCH
             $SUDO docker build --rm -f "$dir/Dockerfile-gpu" -t $REPO/kf-$dir-gpu-$ARCH:latest --build-arg REPO=$REPO --build-arg ARCH=$ARCH --build-arg SOURCE_LIST=$SOURCE_LIST --build-arg TRUSTED_SOURCE=$TRUSTED_SOURCE --build-arg HTTP_PROXY=$PROXY --build-arg HTTPS_PROXY=$PROXY --build-arg HADOOP_URL=$HADOOP_URL "$dir"
+            $SUDO docker build --rm -f "$dir/Dockerfile-cpu" -t $REPO/kf-$dir-cpu-$ARCH:latest --build-arg REPO=$REPO --build-arg ARCH=$ARCH --build-arg SOURCE_LIST=$SOURCE_LIST --build-arg TRUSTED_SOURCE=$TRUSTED_SOURCE --build-arg HTTP_PROXY=$PROXY --build-arg HTTPS_PROXY=$PROXY --build-arg HADOOP_URL=$HADOOP_URL "$dir"
         elif [ "$dir" == "rapids-notebook" ]
         then
             echo $ARCH
@@ -38,6 +39,7 @@ do
         if [ "$dir" == "machine-learning-notebook" ]
         then
             $SUDO docker push $REPO/kf-$dir-gpu-$ARCH:latest
+            $SUDO docker push $REPO/kf-$dir-cpu-$ARCH:latest
         else
             $SUDO docker push $REPO/kf-$dir-$ARCH:latest
         fi
@@ -77,6 +79,10 @@ do
             $SUDO docker manifest annotate $REPO/kf-$dir-gpu:latest $REPO/kf-$dir-gpu-amd64:latest --os linux --arch amd64 
             $SUDO docker manifest annotate $REPO/kf-$dir-gpu:latest $REPO/kf-$dir-gpu-ppc64le:latest --os linux --arch ppc64le
             $SUDO docker manifest push $REPO/kf-$dir-gpu:latest 
+            $SUDO docker manifest create $REPO/kf-$dir-cpu:latest $REPO/kf-$dir-cpu-amd64:latest $REPO/kf-$dir-cpu-ppc64le:latest --amend
+            $SUDO docker manifest annotate $REPO/kf-$dir-cpu:latest $REPO/kf-$dir-cpu-amd64:latest --os linux --arch amd64 
+            $SUDO docker manifest annotate $REPO/kf-$dir-cpu:latest $REPO/kf-$dir-cpu-ppc64le:latest --os linux --arch ppc64le
+            $SUDO docker manifest push $REPO/kf-$dir-cpu:latest 
 
         else
             $SUDO docker manifest create $REPO/kf-$dir:latest $REPO/kf-$dir-amd64:latest $REPO/kf-$dir-ppc64le:latest --amend
